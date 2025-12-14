cbf8b6850621fdd65622c54dd0e42d0031d102d6
Tiago França <github@tiagofranca.com>
2025-12-13 21:20:19 -0300

    removed log on app.vue 	modified:   app/app.vue

--- app/app.vue
+++ app/app.vue
@@ -5,8 +5,6 @@
 <script setup>
 const appConfig = useAppConfig();
 
-console.log('appConfig', appConfig);
-
 // async function submit () {
 //   const { body } = await $fetch('/api/submit', {
 //     method: 'post',
